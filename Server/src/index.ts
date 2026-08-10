c7106ced0e8c88d16449e76eb7e8013875a0cf25
yanir <yanir.laty@gmail.com>
2026-08-10 10:54:13 +0300

    updated cors url for local prod testing

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
+  "http://localhost",
   process.env.BASE_FRONTEND_URL,
 ].filter(Boolean);
 
